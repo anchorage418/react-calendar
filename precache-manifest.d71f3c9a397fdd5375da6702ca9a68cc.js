5a240f7c33327611336bd9f5beb4935a28c48ab7
Denis <lagodnuy.d.virtue@gmail.com>
2018-11-26 14:53:19 +0200

    Updates

---
+++ precache-manifest.d71f3c9a397fdd5375da6702ca9a68cc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "820768afa52bb8f1a0b4",
+    "url": "/react-calendar/static/js/runtime~main.820768af.js"
+  },
+  {
+    "revision": "1f2b9a1c4115e527b05b",
+    "url": "/react-calendar/static/js/main.1f2b9a1c.chunk.js"
+  },
+  {
+    "revision": "32d4ae28c3474573f8ad",
+    "url": "/react-calendar/static/js/1.32d4ae28.chunk.js"
+  },
+  {
+    "revision": "698a3ffa36e142ea587e52fd63c26d9d",
+    "url": "/react-calendar/index.html"
+  }
+];
